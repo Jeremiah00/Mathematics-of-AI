a7b6518143a9fb53d33fa35280236365d70f7b10
Jeremiah00 <jeremiah.w.wade567@gmail.com>
2023-12-05 11:41:28 -0800

    On vectors

---
+++ MathematicsOfAIWadeP4/Assets/Move.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    Vector3 goal = new Vector3(5,0,4);
+    public float speed = 0.1f;
+
+    void Start() {
+
+        goal = goal * speed;
+    }
+
+    void LateUpdate() 
+    {
+        this.transform.Translate(goal.normalized * speed * Time.deltaTime);
+    }
+}
---
+++ MathematicsOfAIWadeP4/Assets/MoveToGoal.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToGoal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform goal;
+    public float speed = 0.1f;
+    public float accuracy = 0.8f;
+
+    void Start()
+    {
+
+
+    }
+
+    void LateUpdate()
+    {
+        Vector3 direction = goal.position - this.transform.position;
+        if(direction.magnitude > accuracy)
+        {
+            this.transform.Translate(direction * speed * Time.deltaTime);
+        }
+        
+    }
+}
